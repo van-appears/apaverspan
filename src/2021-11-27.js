3901ceeedb8fe7cf4c67d7c0b17b1e10b6f2723d
Gareth Robinson <garethr1@yahoo.co.uk>
2021-11-27 19:59:39 +0000

    Process 2021-11-27

---
+++ src/2021-11-27.js
@@ -0,0 +1,29 @@
+const {
+  attributeSorter,
+  emptyData,
+  fillCircle,
+  loadImg,
+  collectColours
+} = require("./tools");
+
+const img = emptyData();
+const filler = fillCircle(img);
+const { width, height } = img;
+const sourceCols = collectColours({
+  img: loadImg("trees1.png"),
+  xlimit: width,
+  ylimit: height
+}).sort(attributeSorter(["v", "s", "h"]));
+
+const maxV = sourceCols.slice(-1)[0].v;
+const sizeFns = [v => 1 + (maxV - v) / 2, v => v / 2];
+
+sizeFns.forEach((sizeFn, i) => {
+  sourceCols.forEach(sourceCol => {
+    const { x, y, v } = sourceCol;
+    const size = sizeFn(v);
+    filler(x, y, size, sourceCol);
+  });
+
+  img.writeOut(__filename, i + 1);
+});

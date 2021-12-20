0012fd3639dfd262f44245ef8454ad4f2a5b09d4
Gareth Robinson <garethr1@yahoo.co.uk>
2021-12-20 22:15:06 +0000

    Process 2021-12-20

---
+++ src/2021-12-20.js
@@ -0,0 +1,26 @@
+const {
+  attributeSorter,
+  collectColours,
+  emptyData,
+  loadImg
+} = require("./tools");
+
+const img = emptyData();
+const { width, height } = img;
+const sorter = attributeSorter(["g", "b", "r"]);
+
+const allColours = collectColours({
+  img: loadImg("trees1.png"),
+  mode: "column"
+});
+
+const pair = (x, y) => [allColours[x][height - 1 - y], allColours[y][x]];
+
+for (x = 0; x < width; x++) {
+  for (let y = 0; y < height; y++) {
+    const keep = pair(x, y).sort(sorter)[0];
+    img.set(x, y, keep);
+  }
+}
+
+img.writeOut(__filename);

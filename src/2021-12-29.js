a5380ebc9da08564c405f4ae3dcd62fa4e4b56a7
Gareth Robinson <garethr1@yahoo.co.uk>
2021-12-29 20:31:46 +0000

    Process 2021-12-29

---
+++ src/2021-12-29.js
@@ -0,0 +1,20 @@
+const { emptyData, loadImg, collectColours } = require("./tools");
+
+const img = emptyData();
+const { width, height } = img;
+const sourceCols = collectColours({
+  img: loadImg("flowers1.png")
+});
+
+const degreeToRad = degree => (2 * Math.PI * degree) / 360;
+const sin = degree => Math.sin(degreeToRad(degree));
+const cos = degree => Math.cos(degreeToRad(degree));
+
+sourceCols.forEach(sourceCol => {
+  const { x, y, h, v } = sourceCol;
+  const newx = x + Math.floor((100 + v) * sin(h));
+  const newy = y + Math.floor((100 + v) * cos(h));
+  img.set(newx, newy, sourceCol);
+});
+
+img.writeOut(__filename);

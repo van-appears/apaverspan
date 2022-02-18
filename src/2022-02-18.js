1446a8938976ca3c7a53b5a74fd46adac242f1e7
Gareth Robinson <garethr1@yahoo.co.uk>
2022-02-18 20:27:25 +0000

    Process 2022-02-18

---
+++ src/2022-02-18.js
@@ -0,0 +1,34 @@
+const {
+  attributeSorter,
+  emptyData,
+  drawLine,
+  loadImg,
+  collectColours
+} = require("./tools");
+
+["shell1.png", "flowers1.png", "location1.png", "flowers2.png" ].forEach(
+  (input, i) => {
+    const source = loadImg(input);
+    const cols = collectColours({
+      img: source
+    }).sort(attributeSorter(["y", "s", "v", "h"]));
+
+    const img = emptyData();
+    const { width, height } = img;
+    const line = drawLine(img);
+
+    const fill = (x, y, col) => {
+      endCol = source.get(x, height - 1);
+      for (let i = -10; i < 11; i++) {
+        line(x + i, y, x + i, height - 1, col, endCol);
+      }
+    };
+
+    for (let i = 0; i < cols.length; i += width + 1) {
+      const { x, y, ...col } = cols[i];
+      fill(x, y, col);
+    }
+
+    img.writeOut(__filename, i + 1);
+  }
+);

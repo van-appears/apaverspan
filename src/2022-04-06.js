24371f6edbfdd7571feb3837a0f1a0fd909347a3
Gareth Robinson <garethr1@yahoo.co.uk>
2022-04-06 23:25:20 +0100

    Process 2022-04-06

---
+++ src/2022-04-06.js
@@ -0,0 +1,24 @@
+const { collectColours, colours, emptyData, loadImg } = require("./tools");
+const {
+  TURN_CLOCKWISE,
+  TURN_ANTICLOCKWISE,
+  LEFT,
+  MIX_COL,
+  run
+} = require("./process/follow-instructions");
+
+["flowers4.png", "flowers1.png", "flowers2.png"].forEach((x, i) => {
+  const img = emptyData();
+  const source = loadImg(x);
+  const instructionData = collectColours({ img: source });
+  run({
+    data: instructionData.slice(0, 30000),
+    mapCol: col => Math.floor(col.h) % 4,
+    instructions: [TURN_CLOCKWISE, TURN_ANTICLOCKWISE, LEFT, MIX_COL],
+    initialWidth: 3,
+    source,
+    img
+  });
+
+  img.writeOut(__filename, i+1);
+});
--- src/process/follow-instructions.js
+++ src/process/follow-instructions.js
@@ -84,13 +84,14 @@ const run = function ({
   data,
   img,
   startx,
-  starty
+  starty,
+  initialWidth = 1
 }) {
   const { width, height } = img;
   const line = drawLine(img);
   const wrapX = xx => (xx < 0 ? xx + width : xx > width ? xx - width : xx);
   const wrapY = yy => (yy < 0 ? yy + height : yy > height ? yy - height : yy);
-  let lineWidth = 1;
+  let lineWidth = initialWidth;
   let x = startx !== undefined ? startx : width / 2;
   let y = starty !== undefined ? starty : height - 1;
   let radian = Math.PI;

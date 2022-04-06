637d456c40a345fc81ba95cd6f875e306e71592d
Gareth Robinson <garethr1@yahoo.co.uk>
2022-04-06 23:26:05 +0100

    Process 2022-04-03 forgot

---
+++ src/2022-04-03.js
@@ -0,0 +1,15 @@
+const { collectColours, colours, emptyData, loadImg } = require("./tools");
+const { run } = require("./process/follow-instructions");
+
+["flowers1.png", "flowers2.png", "flowers4.png"].forEach((x, i) => {
+  const img = emptyData();
+  const source = loadImg(x);
+  const instructionData = collectColours({ img: source });
+  run({
+    data: instructionData.slice(0, 300000),
+    source,
+    img
+  });
+
+  img.writeOut(__filename, i + 1);
+});

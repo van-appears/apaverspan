83c06e36032c75c34112b4dfd6b2ede9e9c483a0
Gareth Robinson <garethr1@yahoo.co.uk>
2022-04-26 23:10:57 +0100

    Adding run-all script

---
+++ run-all.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+const childProcess = require("child_process");
+
+function processor(stillToProcess) {
+  if (stillToProcess.length > 0) {
+    const processFile = stillToProcess[0];
+    const startTime = new Date().getTime();
+    const child = childProcess.fork(processFile);
+    child.on("exit", () => {
+      const endTime = new Date().getTime();
+      const seconds = Math.round((endTime - startTime) / 1000);
+      console.log(`Finished ${processFile} in ${seconds}s`);
+      processor(stillToProcess.slice(1));
+    });
+  }
+}
+
+const srcDir = path.join(".", "src");
+const dateProcess = new RegExp(`[0-9]{4}-[0-9]{2}-[0-9]{2}.js`);
+const processFiles = fs
+  .readdirSync(srcDir)
+  .filter(x => dateProcess.test(x))
+  .map(x => path.join(srcDir, x));
+processor(processFiles);

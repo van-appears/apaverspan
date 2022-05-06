8973e213ed92ecb5abda653551314195eaea8d04
Gareth Robinson <garethr1@yahoo.co.uk>
2022-05-06 22:05:24 +0100

    Process 2022-05-06

---
+++ src/2022-05-06.js
@@ -0,0 +1,26 @@
+const {
+  attributeSorter,
+  emptyData,
+  loadImg,
+  collectColours
+} = require("./tools");
+const img = emptyData();
+
+["flowers1.png", "shell1.png"].forEach((file, i) => {
+  const source = loadImg(file);
+  const { height, width } = img;
+  const allColours = collectColours({ img: source }).sort(
+    attributeSorter(["h", "v", "s"])
+  );
+
+  const mappedColours = allColours
+    .map(({ x, y }, i) => {
+      const moved = allColours[(i + 500000) % allColours.length];
+      return { ...moved, x, y };
+    })
+    .forEach(({ x, y, ...col }) => {
+      img.set(x, y, col);
+    });
+
+  img.writeOut(__filename, i + 1);
+});

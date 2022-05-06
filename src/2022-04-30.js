20da20d0dc9fb81d0d946748cebc5f7b738a0f63
Gareth Robinson <garethr1@yahoo.co.uk>
2022-05-06 22:04:31 +0100

    Process 2022-04-30

--- src/2022-04-30.js
+++ src/2022-04-30.js
@@ -1,19 +1,25 @@
-const { attributeSorter, collectColours, emptyData, loadImg, zoomIn } = require("./tools");
+const {
+  attributeSorter,
+  collectColours,
+  emptyData,
+  loadImg,
+  zoomIn
+} = require("./tools");
 const img = emptyData();
 
 const source = loadImg("trees1.png");
 const { height, width } = img;
-const allColours = collectColours({ img: source })
-  .sort(attributeSorter(['s', 'v', 'h']));
+const allColours = collectColours({ img: source }).sort(
+  attributeSorter(["s", "v", "h"])
+);
 
 allColours
-  .map(({x, y}, i) => {
+  .map(({ x, y }, i) => {
     const moved = allColours[(i + 600000) % allColours.length];
     return { ...moved, x, y };
   })
-  .forEach(({x, y, ...col}) => {
+  .forEach(({ x, y, ...col }) => {
     img.set(x, y, col);
   });
 
-zoomIn(img, 300, 0, width - 300, height - 300)
-  .writeOut(__filename);
+zoomIn(img, 300, 0, width - 300, height - 300).writeOut(__filename);

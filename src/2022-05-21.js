1f3e7c57615710ddbe8dc4bf079ef2ad3a5770b7
Gareth Robinson <garethr1@yahoo.co.uk>
2022-05-29 10:08:36 +0100

    Process 2022-05-21 fix

--- src/2022-05-21.js
+++ src/2022-05-21.js
@@ -1,9 +1,10 @@
 const combineImagesPixels = require("./process/combine-images-pixels");
 const convert = require("color-convert");
 
-function combiner(colour1, colour2) {
-  const { h: h1, s: s1, v: v1 } = colour1;
-  const { h: h2, s: s2, v: v2 } = colour2;
+function combiner(pixels) {
+  const [pixel1, pixel2] = pixels;
+  const { h: h1, s: s1, v: v1 } = pixel1;
+  const { h: h2, s: s2, v: v2 } = pixel2;
   const h = (h1 * h2) / 360;
   const s = (s1 * s2) / 100;
   const v = (v1 * v2) / 100;

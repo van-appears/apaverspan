220569b05f832a0ff00c928348d6d4b4d023a259
Gareth Robinson <garethr1@yahoo.co.uk>
2022-05-29 09:44:29 +0100

    Process 2022-05-21 extracted process

--- src/2022-05-21.js
+++ src/2022-05-21.js
@@ -1,14 +1,7 @@
-const { collectColours, emptyData, loadImg } = require("./tools");
+const combineImagesPixels = require("./process/combine-images-pixels");
 const convert = require("color-convert");
-const img = emptyData();
-const { width, height } = img;
 
-const pairs = [
-  ["flowers1.png", "flowers2.png"],
-  ["flowers3.png", "flowers4.png"]
-];
-
-function combineHSV(colour1, colour2) {
+function combiner(colour1, colour2) {
   const { h: h1, s: s1, v: v1 } = colour1;
   const { h: h2, s: s2, v: v2 } = colour2;
   const h = (h1 * h2) / 360;
@@ -18,20 +11,12 @@ function combineHSV(colour1, colour2) {
   return { r, g, b };
 }
 
-pairs.forEach((pair, i) => {
-  const colours0 = collectColours({
-    img: loadImg(pair[0]),
-    mode: "column"
-  });
-  const colours1 = collectColours({
-    img: loadImg(pair[1]),
-    mode: "column"
-  });
-
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      img.set(x, y, combineHSV(colours0[x][y], colours1[x][y]));
-    }
-  }
-  img.writeOut(__filename, i + 1);
-});
+[
+  ["flowers1.png", "flowers2.png"],
+  ["flowers3.png", "flowers4.png"]
+].forEach((pair, i) =>
+  combineImagesPixels({
+    files: pair,
+    combiner
+  }).writeOut(__filename, i + 1)
+);

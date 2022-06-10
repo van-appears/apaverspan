e6195360b197fbe378a72d78eae3d09031489fe2
Gareth Robinson <garethr1@yahoo.co.uk>
2022-06-10 18:22:52 +0100

    Small format change

--- src/tools/quadrant-zoom.js
+++ src/tools/quadrant-zoom.js
@@ -2,11 +2,11 @@ const zoomIn = require("./zoom-in");
 const constants = require("./constants");
 const half = constants.IMAGE_SIZE / 2;
 
-module.exports = function(source, interpolate) {
+module.exports = function (source, interpolate) {
   return [
     zoomIn(source, 0, 0, half, half, interpolate),
     zoomIn(source, half, 0, half, half, interpolate),
     zoomIn(source, 0, half, half, half, interpolate),
     zoomIn(source, half, half, half, half, interpolate)
   ];
-}
+};

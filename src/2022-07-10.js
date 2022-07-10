d5d4347007728fbdbd13e02fde1f70b4a7ee44d2
Gareth Robinson <garethr1@yahoo.co.uk>
2022-07-10 21:12:41 +0100

    Process 2022-07-10

---
+++ src/2022-07-10.js
@@ -0,0 +1,22 @@
+const { collectColours, drawLine, emptyData } = require("./tools");
+
+const img = emptyData();
+const { width, height } = img;
+const hsMap = collectColours({ file: "flowers1.png" }).reduce((acc, col) => {
+  const { h, s } = col;
+  const key = `${h}_${s}`;
+  const collected = (acc[key] = acc[key] || []);
+  collected.push(col);
+  return acc;
+}, {});
+
+Object.values(hsMap).forEach(colGroup => {
+  const len = colGroup.length;
+  const avgX = colGroup.reduce((acc, col) => acc + col.x, 0) / len;
+  const avgY = colGroup.reduce((acc, col) => acc + col.y, 0) / len;
+  colGroup.forEach(({ x, y, ...col }) => {
+    img.set(Math.round((avgX + x) / 2), Math.round((avgY + y) / 2), col);
+  });
+});
+
+img.writeOut(__filename);

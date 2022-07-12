237deee23ca844b5c3904422fef0d8db4ca7c1ed
Gareth Robinson <garethr1@yahoo.co.uk>
2022-07-12 23:12:19 +0100

    Process 2022-07-07 tidy

--- src/2022-07-07.js
+++ src/2022-07-07.js
@@ -1,7 +1,6 @@
-const { collectColours, drawLine, emptyData } = require("./tools");
+const { collectColours, emptyData } = require("./tools");
 
 const img = emptyData();
-const { width, height } = img;
 const vsMap = collectColours({ file: "trees1.png" }).reduce((acc, col) => {
   const { s, v } = col;
   const key = `${v}_${s}`;

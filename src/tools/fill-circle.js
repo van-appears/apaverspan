4d31aa7194f083ece6bed128826f7133dc29f097
Gareth Robinson <garethr1@yahoo.co.uk>
2022-08-07 10:03:11 +0100

    Adding wrap parameter to fillRect and fillCircle

--- src/tools/fill-circle.js
+++ src/tools/fill-circle.js
@@ -1,22 +1,35 @@
+const wrapPosition = require("./wrap-position");
+
 module.exports = function (img) {
-  const filler = (x, y, radius, colourOrFun) => {
+  const { wrapX, wrapY } = wrapPosition(img);
+
+  const filler = (x, y, radius, colourOrFun, wrap) => {
     const r2 = radius * radius;
     const isColourFn = typeof colourOrFun === "function";
     for (let xi = -radius; xi <= radius; xi++) {
       for (let yi = -radius; yi <= radius; yi++) {
         if (xi * xi + yi * yi <= r2) {
+          const thisX = wrap ? wrapX(x + xi) : x + xi;
+          const thisY = wrap ? wrapY(y + yi) : y + yi;
+
           if (isColourFn) {
-            const previous = img.get(x + xi, y + yi);
-            img.set(x + xi, y + yi, colourOrFun(x + xi, y + yi, previous));
+            const previous = img.get(thisX, thisY);
+            img.set(thisX, thisY, colourOrFun(previous, thisX, thisY));
           } else {
-            img.set(x + xi, y + yi, colourOrFun);
+            img.set(thisX, thisY, colourOrFun);
           }
         }
       }
     }
   };
-  if (arguments.length === 5) {
-    filler(arguments[1], arguments[2], arguments[3], arguments[4]);
+  if (arguments.length >= 5) {
+    filler(
+      arguments[1],
+      arguments[2],
+      arguments[3],
+      arguments[4],
+      arguments[5]
+    );
   }
   return filler;
 };
--- src/tools/fill-rect.js
+++ src/tools/fill-rect.js
@@ -1,18 +1,32 @@
+const wrapPosition = require("./wrap-position");
+
 module.exports = function (img) {
-  const filler = (x, y, width, height, colour) => {
+  const { wrapX, wrapY } = wrapPosition(img);
+
+  const filler = (x, y, width, height, colourOrFun, wrap) => {
+    const isColourFn = typeof colourOrFun === "function";
     for (let xi = 0; xi < width; xi++) {
       for (let yi = 0; yi < height; yi++) {
-        img.set(x + xi, y + yi, colour);
+        const thisX = wrap ? wrapX(x + xi) : x + xi;
+        const thisY = wrap ? wrapY(y + yi) : y + yi;
+
+        if (isColourFn) {
+          const previous = img.get(thisX, thisY);
+          img.set(thisX, thisY, colourOrFun(previous, thisX, thisY));
+        } else {
+          img.set(thisX, thisY, colourOrFun);
+        }
       }
     }
   };
-  if (arguments.length === 6) {
+  if (arguments.length >= 6) {
     filler(
       arguments[1],
       arguments[2],
       arguments[3],
       arguments[4],
-      arguments[5]
+      arguments[5],
+      arguments[6]
     );
   }
   return filler;

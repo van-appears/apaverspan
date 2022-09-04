ec3f214341995b3d95f18c22ea9d0ce4f985caf2
Gareth Robinson <garethr1@yahoo.co.uk>
2022-09-04 21:59:11 +0100

    Adding prototyping constant for all source files

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -2,5 +2,14 @@ const path = require("path");
 
 module.exports = {
   IMAGE_SIZE: 1400,
-  INPUT_DIR: path.join(__dirname, "..", "..", "in")
+  INPUT_DIR: path.join(__dirname, "..", "..", "in"),
+  ALL_FILES: [
+    "flowers1.png",
+    "flowers2.png",
+    "flowers3.png",
+    "flowers4.png",
+    "location1.png",
+    "shell1.png",
+    "trees1.png"
+  ]
 };

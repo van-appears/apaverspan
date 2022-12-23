0f262e3178ca383d14ffe71c58536b193850848c
Van Appears <garethr1@yahoo.co.uk>
2022-12-23 22:36:53 +0000

    Process 2022-12-23

---
+++ src/2022-12-23.js
@@ -0,0 +1,25 @@
+const { asNum, colours, loadImg, random } = require("./tools");
+const { rndBoolean } = random(asNum(__filename));
+
+const img = rotations.clockwise90(loadImg("flowers2.png"));
+const { width, height } = img;
+
+for (let y = 1; y < height; y++) {
+  for (let x = 0; x < width - y; x++) {
+    const col1 = img.get(x, y + x);
+    const col2 = img.get(x, y + x - 1);
+    const col3 = img.get(x + 1, y + x);
+    img.set(x, y + x, rndBoolean() ? col3 : colours.avg(col2, col3));
+  }
+}
+
+for (let x = 1; x < width; x++) {
+  for (let y = 0; y < height - x; y++) {
+    const col1 = img.get(x + y, y);
+    const col2 = img.get(x + y - 1, y);
+    const col3 = img.get(x + y, y + 1);
+    img.set(x + y, y, rndBoolean() ? col2 : colours.avg(col2, col3));
+  }
+}
+
+img.writeOut(__filename);

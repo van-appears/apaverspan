1646bdaa27fa66e15b0bfbb49421d8d0a1ea6170
Van Appears <garethr1@yahoo.co.uk>
2023-01-02 22:33:24 +0000

    Process 2023-01-02

---
+++ src/2023-01-02.js
@@ -0,0 +1,54 @@
+const { asNum, loadImg, random } = require("./tools");
+const { int, item } = random(asNum(__filename));
+const asKey = ({ x, y }) => `${x}_${y}`;
+
+["flowers4.png", "trees1.png", "flowers1.png"].forEach((file, i) => {
+  const img = loadImg(file);
+  const { width, height } = img;
+
+  for (let loop = 0; loop < 100000; loop++) {
+    let x = int(width);
+    let y = int(height);
+    let attr = {};
+    switch (int(3)) {
+      case 0:
+        attr.r = img.get(x, y).r;
+        break;
+      case 1:
+        attr.g = img.get(x, y).g;
+        break;
+      case 2:
+        attr.b = img.get(x, y).b;
+        break;
+    }
+
+    const visited = {};
+    let keepGoing = true;
+    while (keepGoing) {
+      const possible = [
+        { x: x - 1, y: y - 1 },
+        { x: x - 1, y },
+        { x: x - 1, y: y + 1 },
+        { x, y: y + 1 },
+        { x: x + 1, y: y + 1 },
+        { x: x + 1, y },
+        { x: x + 1, y: y - 1 },
+        { x, y: y - 1 }
+      ].filter(pos => !visited[asKey(pos)]);
+
+      keepGoing = false;
+      if (possible.length) {
+        const next = item(possible);
+        if (next.x >= 0 && next.x < width && next.y >= 0 && next.y < height) {
+          const col = img.get(next.x, next.y);
+          img.set(next.x, next.y, { ...col, ...attr });
+          keepGoing = visited[asKey(next)] = true;
+          x = next.x;
+          y = next.y;
+        }
+      }
+    }
+  }
+
+  img.writeOut(__filename, i + 1);
+});

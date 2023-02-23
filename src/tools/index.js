006f2c8116c94c2c48b4eb7c3f04ef9741f2dc34
Van Appears <garethr1@yahoo.co.uk>
2023-02-23 21:22:20 +0000

    Adding convert functions

--- src/tools/index.js
+++ src/tools/index.js
@@ -5,7 +5,7 @@ module.exports = {
   collectColours: require("./collect-colours"),
   colours: require("./colours"),
   constants: require("./constants"),
-  convert:  require("./convert"),
+  convert: require("./convert"),
   drawLine: require("./draw-line"),
   emptyData: require("./empty-data"),
   fillCircle: require("./fill-circle"),

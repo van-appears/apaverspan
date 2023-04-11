2691186fc9dcbb4c7561ce948abfaeb4110c751e
Van Appears <garethr1@yahoo.co.uk>
2023-04-11 09:52:52 +0100

    Process 2023-12-23 tidy

--- src/2022/2022-12-23.js
+++ src/2022/2022-12-23.js
@@ -6,19 +6,17 @@ const { width, height } = img;
 
 for (let y = 1; y < height; y++) {
   for (let x = 0; x < width - y; x++) {
-    const col1 = img.get(x, y + x);
-    const col2 = img.get(x, y + x - 1);
-    const col3 = img.get(x + 1, y + x);
-    img.set(x, y + x, rndBoolean() ? col3 : colours.avg(col2, col3));
+    const col1 = img.get(x, y + x - 1);
+    const col2 = img.get(x + 1, y + x);
+    img.set(x, y + x, rndBoolean() ? col2 : colours.avg(col1, col2));
   }
 }
 
 for (let x = 1; x < width; x++) {
   for (let y = 0; y < height - x; y++) {
-    const col1 = img.get(x + y, y);
-    const col2 = img.get(x + y - 1, y);
-    const col3 = img.get(x + y, y + 1);
-    img.set(x + y, y, rndBoolean() ? col2 : colours.avg(col2, col3));
+    const col1 = img.get(x + y - 1, y);
+    const col2 = img.get(x + y, y + 1);
+    img.set(x + y, y, rndBoolean() ? col1 : colours.avg(col1, col2));
   }
 }
 

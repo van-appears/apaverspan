04d2b40efb2e19a219842f35ac6df4cec762927c
Van Appears <garethr1@yahoo.co.uk>
2023-05-05 22:23:11 +0100

    Process 2022-12-16 tidy

--- src/2022-12-16.js
+++ src/2022-12-16.js
@@ -7,7 +7,6 @@ const { clockwise90 } = rotations;
 
   for (let y = 1; y < height; y++) {
     for (let x = 0; x < width - y; x++) {
-      const col1 = img.get(x, y + x);
       const col2 = img.get(x, y + x - 1);
       const col3 = img.get(x + 1, y + x);
       img.set(x, y + x, colours.avg(col2, col3));
@@ -16,7 +15,6 @@ const { clockwise90 } = rotations;
 
   for (let x = 1; x < width; x++) {
     for (let y = 0; y < height - x; y++) {
-      const col1 = img.get(x + y, y);
       const col2 = img.get(x + y - 1, y);
       const col3 = img.get(x + y, y + 1);
       img.set(x + y, y, colours.avg(col2, col3));
